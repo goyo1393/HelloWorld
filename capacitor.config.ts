fcef4fe3031ef73d56349dda0e1cf5aedf6ebe4a
Jose Escobar <jgescobarquintero@gmail.com>
2024-09-03 20:10:14 +0200

    Cambios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'HelloWorld',
+  appId: 'com.jgescobar.taskmanager',
+  appName: 'EasyTaskManager',
   webDir: 'www'
 };
 
